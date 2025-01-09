d95854cee2577cf23659727abb2d85847bd7f2c6
Michael Palmer <michaelpalmer53@gmail.com>
2025-01-09 21:02:38 +0000

    adds creation/updating of results with aPI

--- index.js
+++ index.js
@@ -1,69 +1,65 @@
-const fs = require('fs');
+const startTime = performance.now();
 const axios = require('axios');
+const { setup } = require('./setup');
+const { join } = require('path');
 
 const runtime = async (context, fn) => {
-    // log result
     return fn(context);
 }
 
 const main = async () => {
-    // load action.json
-    const action = require('./action.json');
     const errors = [];
 
-    const token = fs.readFileSync('token');
-
-    const startTime = performance.now();
-
-    // load userdata from Auth0
-    // TODO: use user token/refresh token instead of machine token?
-    // this would reduce number of machine tokens + ensure user is authenticated.
-    let response;
-    let user;
+    let sdata;
     try {
-        response = await axios.get('https://id.myked.io/userinfo', { headers: {Authorization: `Bearer ${token}`}});
-        user = response.data;
+        sdata = await setup(join(__dirname, 'config.json'));
+    } catch (error) {
+        errors.push(error, new Error('initial setup failed'));
+    }
+
+    // create action result with API
+    const { config, ...data } = sdata;
+    let result;
+    try {
+        result = (await axios.post(`${config.api_base_url}/results`, {
+            action_id: data.context.action.action_id,
+            ...data
+        }, {
+            headers: { Authorization: `Bearer ${sdata.config.token}` }
+        })).data;
     } catch (error) {
         errors.push(error);
     }
 
-    const context = {
-        user,
-        action,
-        variables: action.variables,
-        secrets: action.secrets,
-    };
-
-    // load fn from action.js
-    let fn;
-    try {
-        fn = require('./action')[action.entry];
-        context['fn'] = fn;
-    } catch (error) {
-        errors.push(error);
+    // handle setup failures
+    if (errors.length !== 0) {
+        result.duration = performance.now() - startTime;
+        throw new Error('initial setup has failed', errors);
     }
 
     // get result
     let dataJson = null;
     try {
-        const result = await runtime(context, fn);
+        const fnResult = await runtime(result.context, sdata.context.fn);
 
         // store it
-        dataJson = JSON.stringify(result);
+        dataJson = JSON.stringify(fnResult);
     } catch (error) {
         errors.push(error);
     }
 
     const endTime = performance.now();
 
-    const result = {
-        context,
-        data: dataJson,
-        errors: errors,
-        status: errors.length !== 0 ? 'failed' : 'completed',
-        duration: endTime - startTime
-    };
+    result.data = dataJson;
+    result.errors = errors;
+    result.duration = endTime - startTime;
+    result.status = errors.length !== 0 ? 'failed' : 'completed';
 
-    console.log(JSON.stringify(result));
+    console.log(result);
+
+    const { result_id, context, ...rest } = result;
+    await axios.patch(`${config.api_base_url}/results/${result.result_id}`, rest, {
+        headers: { Authorization: `Bearer ${config.token}`}
+    });
 };
 main();
