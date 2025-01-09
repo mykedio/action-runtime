cc66b7dc8d73e7b950827d74a61b06f7b2a8e998
Michael Palmer <michaelpalmer53@gmail.com>
2025-01-09 21:55:16 +0000

    removes api calls from runtime (provided by builder instead)

--- setup.js
+++ setup.js
@@ -15,31 +15,14 @@ exports.setup = async (path) => {
         throw new Error('config file improperly formatted');
     }
 
-    const headers = {
-        Authorization: `Bearer ${config.token}`
-    }
-
     // get user data
-    const user = (await axios.get(`${config.id_base_url}/userinfo`, { headers })).data;
+    const user = config.user;
 
     // get action data
-    const action = (await axios.get(`${config.api_base_url}/actions/${config.action_id}`, { headers })).data;
+    const action = config.action;
 
     if (!action.hash) {
         throw new Error('no code has been uploaded');
-    }
-
-    // get user code from action (if exists)
-    const usercode = (await axios.get(`${config.api_base_url}/actions/${config.action_id}/usercode`, { headers })).data.user_code;
-    writeFileSync(join(__dirname, 'action.js'), Buffer.from(usercode, 'base64'));
-
-    // check hashes
-    const usercodeBuf = Buffer.from(usercode, 'base64');
-    const hash = Buffer.from(action.hash, 'base64');
-    const cmp = createHash('sha256').update(usercodeBuf).digest();
-
-    if (!hash.equals(cmp)) {
-        throw new Error('usercode failed checksum match');
     }
 
     return {
